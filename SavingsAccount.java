c307025c4d592e0c8f97ba8c2c255c010bf1642e
group004 <group004@infoway.com>
2019-12-03 15:14:05 +0530

    Bank work

---
+++ SavingsAccount.java
@@ -0,0 +1,31 @@
+package assignment_47;
+
+public class SavingsAccount extends TheBank{
+	int accountno;
+	String name;
+	double balance;
+	String branch;
+	public SavingsAccount()
+	{}
+	public SavingsAccount(int accountno,String name,double balance,String branch)
+	{
+		this.accountno=accountno;
+		this.name=name;
+		this.balance=balance;
+		this.branch=branch;
+		}
+	public void display2()
+	{
+		System.out.println("Account no= "+accountno+" Name: "+name+" balance: "+branch);
+		}
+	public void withdrawl(double amountw)
+	{
+		double with=balance-amountw;
+		System.out.println("After withdrawl,remaining balance: "+with);
+		}
+	public void deposit(double amountd)
+	{
+		double dep=balance+amountd;
+		System.out.println("After depositing,total account balance: "+dep);
+		}
+}
---
+++ TheBank.java
@@ -0,0 +1,6 @@
+package assignment_47;
+
+public abstract class TheBank {
+public abstract void withdrawl(double amountw);
+public abstract void deposit(double amountd);
+}
